e5f468562291689c40e4b7451614df6b18505bf3
kmattingly8 <kirsten.mattingly@icloud.com>
2025-02-15 16:30:18 -0800

    Updated App Port for Heroke

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import joblib
 import numpy as np
+import os
 
 # Initialize the Flask app
 app = Flask(__name__)
@@ -31,4 +32,6 @@ def predict():
     return render_template('index.html', prediction=result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Get the PORT environment variable, default to 5000 if not provided
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
